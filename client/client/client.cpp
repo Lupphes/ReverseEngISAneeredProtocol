26542fa0d45f0d3620d513bf5348c21ed52bc5dc
Ondřej Sloup <ondrej.sloup@protonmail.com>
2021-10-08 01:46:35 +0200

    Add client project

---
+++ client/client/client.cpp
@@ -0,0 +1,15 @@
+﻿// client.cpp : Defines the entry point for the application.
+//
+
+#include "client.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
